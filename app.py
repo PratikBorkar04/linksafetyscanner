8adfcb01adddab627dc94bc43190589750818f92
Pratik Borkar <pratikab01@gmail.com>
2024-01-18 15:44:46 +0530

    logger and Exception Created

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-
+from linksafety.logger import logging
+from linksafety.exception import LinksafetyException
+import sys
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception("Custom EXCEPTION TESTING")
+    except Exception as ex:
+        linksafety = LinksafetyException(ex,sys)
+        logging.info(linksafety.error_message)
+        logging.info("Testing Logging")
     return "Machine Learning Project"
 
 if __name__=="__main__":
