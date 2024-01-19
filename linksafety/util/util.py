d254c099efafcc5af0210a14ed162e220f38ed94
Pratik Borkar <pratikab01@gmail.com>
2024-01-19 14:54:24 +0530

    Templates added

---
+++ linksafety/util/util.py
@@ -0,0 +1,14 @@
+import yaml
+from linksafety.exception import LinksafetyException
+import sys
+from linksafety.constant import *
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as ex:
+        raise LinksafetyException(e,sys) from ex
